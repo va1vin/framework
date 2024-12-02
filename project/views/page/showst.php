086e11f11a20d0c3476427da054250c94928f980
val <vinval1999@gmail.com>
2024-12-02 12:34:03 +0400

    Ресурсы

--- project/views/page/showst.php
+++ project/views/page/showst.php
@@ -5,7 +5,10 @@
     <title><?= $title ?></title>
 </head>
 <body>
-    <h1><?= $title ?></h1>
-    <p><?= $text ?></p>
+    <div class="container">
+        <h1><?= $title ?></h1>
+        <img src="/public/images/3F3F3F_3F.29.webp" alt="Пример картинки" style="max-width: 100%; height: auto;">
+        <p><?= $text ?></p>
+    </div>
 </body>
 </html>
