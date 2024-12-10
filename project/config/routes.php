cd61a3295d54b17a37c0f05f4380a296269089ac
val <vinval1999@gmail.com>
2024-12-10 13:23:49 +0400

    1

--- project/config/routes.php
+++ project/config/routes.php
@@ -6,7 +6,7 @@ return [
 	new Route('/my-page1/', 'page', 'show1'),
 	new Route('/my-page2/', 'page', 'show2'),
 	new Route('/page/showst/:id/', 'page', 'showst'),
-	new Route('/page/act', 'page', 'act'),
+	new Route('/page/test', 'page', 'test'),
 	new Route('/act1/', 'test', 'act1'),
 	new Route('/act2/', 'test', 'act2'),
 	new Route('/act3/', 'test', 'act3'),
